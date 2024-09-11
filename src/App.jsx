d9de09f2c84710549d982bbc3da7f7bc421fe9ce
nahidaHassan <nahidahassan220@gmail.com>
2024-09-11 16:12:46 +0500

    clonee

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import './App.css';
+import Sidebar from './components/sidebar/sidebar.jsx';
+import Main from './components/main/Main.jsx';
+
+function App() {
+  return (
+    <>
+     <Sidebar/>
+     <Main/>
+     
+    </>
+  );
+}
+
+export default App;
